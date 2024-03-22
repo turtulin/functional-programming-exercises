bc1a884a5e7432534cbae5cc6ea2c4202b32f1aa
turtulin <marta.musso@studenti.unicam.it>
2024-03-22 10:52:54 +0100

    [add] added exercises about recursive functions

--- RecursiveFunctions/recursion.hs
+++ RecursiveFunctions/recursion.hs
@@ -1,3 +1,6 @@
+import Data.Bits
+-- In these exercises we assume that inputs are non-negatives
+
 fibonacci :: Int -> Int
 fibonacci k | k == 0 = 0
             | k == 1 = 1
@@ -7,7 +10,21 @@ powN :: Int -> Int
 powN n | n == 0 = 1
        | n > 0  = 2 * powN (n - 1)
 
--- This is an alternative way to express | 
+bits :: Int -> Int
+bits n | n == 0 = 0
+       | otherwise = (n .&. 1) + bits (n `shiftR` 1)
+
+-- Alternative syntax 
 somma :: Int -> Int
 somma 0 = 0
 somma n = n + somma (n - 1)
+
+powOf2 :: Int -> Bool
+powOf2 n | n == 0 = False
+         | n == 1 = True
+         | even n = powOf2 (n `div` 2)
+         | otherwise = False
+
+myGcd :: Int -> Int -> Int
+myGcd m 0 = m
+myGcd m n = myGcd n (m `mod` n)
