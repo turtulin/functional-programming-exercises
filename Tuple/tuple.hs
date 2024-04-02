28332f352245bbea4665e75fbfdb80e942da0ad6
turtulin <marta.musso@studenti.unicam.it>
2024-04-02 10:18:26 +0200

    [add] added exercises on tuples

--- Tuple/tuple.hs
+++ Tuple/tuple.hs
@@ -11,3 +11,9 @@ distance (x1, y1) (x2, y2) = sqrt (dx ^ 2 + dy ^ 2)
  where
   dx = x1 - x2
   dy = y1 - y2
+
+swap :: (Int, Int) -> (Int, Int)
+swap (x, y) = (y, x)
+
+sort :: (Int, Int, Int) -> (Int, Int, Int)
+sort (x, y, z) = (min x (min y z), x + y + z - min x (min y z) - max x (max y z), max x (max y z))
---
+++ complex.hs
@@ -0,0 +1,22 @@
+type Complex = (Double, Double)
+
+add :: (Complex, Complex) -> Complex
+add ((x1, y1), (x2, y2)) = (x1 + x2, y1 + y2)
+
+myNegate :: Complex -> Complex
+myNegate (x, y) = (negate x, y)
+
+myNegate1 :: Complex -> Complex
+myNegate1 (x, y) = (-x, y)
+
+myConjugate :: Complex -> Complex
+myConjugate (x, y) = (x, negate y)
+
+myConjugate1 :: Complex -> Complex
+myConjugate1 (x, y) = (x, -y)
+
+subtract :: (Complex, Complex) -> Complex
+subtract (x, y) = add (x, myNegate y)
+
+myModule :: Complex -> Double
+myModule (x, y) = sqrt (x ^ 2 + y ^ 2)
