9621ee690c64c2b44f71bf0cb4186d4a8ad584aa
turtulin <marta.musso@studenti.unicam.it>
2024-04-03 19:16:23 +0200

    [add] lists

--- Lists/close.hs
+++ Lists/close.hs
@@ -6,3 +6,9 @@ total :: [Int] -> [Int] -> [(Int, Int)]
 total [] _ = []
 total _ [] = []
 total (x : xs) (y : ys) = (x, y) : total xs ys
+
+open :: [(Int, Int)] -> ([Int], [Int])
+open [] = ([], [])
+open ((x, y) : xs) = (x : ys, y : zs)
+ where
+    (ys, zs) = open xs
